0addf4b5cccf120cf0efd662b19e2361f5ec4e6a
sunpochin <sunpochin@gmail.com>
2025-03-26 09:53:31 +0800

    feat: use tailwind 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  css: [
+    '~/assets/css/tailwind.css',
+  ],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   watchers: {
@@ -15,15 +21,4 @@ export default defineNuxtConfig({
   routeRules: {
     '/': { redirect: '/home' }
   },
-  css: ['~/assets/css/tailwind.css'],
-  postcss: {
-    plugins: {
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
-  },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
-  // Note: The old router.extendRoutes approach is removed as it's not compatible with Nuxt 3
 })
